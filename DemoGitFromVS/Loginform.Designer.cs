85561d7b921ae6123c45b6efb4a94b8d8e131bbf
bidkar <bidkar.aragon@udo.mx>
2018-09-04 11:42:40 -0600

    cambios sugeridos

--- DemoGitFromVS/Loginform.Designer.cs
+++ DemoGitFromVS/Loginform.Designer.cs
@@ -33,12 +33,13 @@
             this.txtcontraseña = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // txtUsuario
             // 
             this.txtUsuario.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.txtUsuario.Location = new System.Drawing.Point(32, 51);
+            this.txtUsuario.Location = new System.Drawing.Point(18, 48);
             this.txtUsuario.Name = "txtUsuario";
             this.txtUsuario.Size = new System.Drawing.Size(190, 20);
             this.txtUsuario.TabIndex = 0;
@@ -47,7 +48,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(82, 14);
+            this.label1.Location = new System.Drawing.Point(76, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(74, 24);
             this.label1.TabIndex = 1;
@@ -55,7 +56,7 @@
             // 
             // txtcontraseña
             // 
-            this.txtcontraseña.Location = new System.Drawing.Point(32, 141);
+            this.txtcontraseña.Location = new System.Drawing.Point(18, 104);
             this.txtcontraseña.Name = "txtcontraseña";
             this.txtcontraseña.PasswordChar = '*';
             this.txtcontraseña.Size = new System.Drawing.Size(190, 20);
@@ -65,7 +66,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(66, 98);
+            this.label2.Location = new System.Drawing.Point(60, 74);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 24);
             this.label2.TabIndex = 3;
@@ -73,25 +74,36 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 203);
+            this.button1.Location = new System.Drawing.Point(52, 153);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 34);
             this.button1.TabIndex = 4;
             this.button1.Text = "Iniciar Sesion";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(33, 130);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(160, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Mantener la sesión iniciada?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Loginform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 281);
+            this.ClientSize = new System.Drawing.Size(232, 222);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtcontraseña);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtUsuario);
             this.Name = "Loginform";
-            this.Text = "Loginform";
+            this.Text = "Inicio de sesión";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,5 +116,6 @@
         private System.Windows.Forms.TextBox txtcontraseña;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
